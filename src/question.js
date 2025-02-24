bbc57b27ca70f5413aaa700461ccc26c76677b93
Larissa Almeida <oa.larissa@hotmail.com>
2025-02-24 17:47:52 +0100

    Partially Solved.Day1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
-}
+    shuffleChoices(){
+
+        for(let i = 0; i < this.choices.length; i++){   
+            const shuffle = Math.round(Math.random() * (i + 1));
+            [this.choices[i], this.choices[shuffle]] = [this.choices[shuffle], this.choices[i]];
+        }
+    }
+
+        }
+  
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,42 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
     
-    // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+   getQuestion() {
+         return this.questions[this.currentQuestionIndex]
+    
+   }
 
-    // 5. checkAnswer(answer)
+    moveToNextQuestion() {
+        this.currentQuestionIndex += 1;
+    }
 
-    // 6. hasEnded()
+    shuffleQuestions() {
+        for(let i = 0; i < this.questions.length; i++){   
+            const shuffle = Math.round(Math.random() * (i + 1));
+            [this.questions[i], this.questions[shuffle]] = [this.questions[shuffle], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer)
+    {
+        if (this.questions[this.currentQuestionIndex].answer === answer){
+            this.correctAnswers += 1;
+        }
+    }
+
+     hasEnded() {
+        if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+}
 }
