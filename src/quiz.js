09c81447428826edce230f7a1c9c0ffd0286d5c0
Jennifer MERAT <131549759+refinejcode49@users.noreply.github.com>
2025-03-02 17:26:28 +0100

    Solved lab

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,57 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+}
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+}
 
-    // 4. shuffleQuestions()
+moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+}
 
-    // 5. checkAnswer(answer)
+shuffleQuestions() {
+    for(let i = 0; i < this.questions.length; i++){   
+        const shuffle = Math.round(Math.random() * (i + 1));
+        [this.questions[i], this.questions[shuffle]] = 
+        [this.questions[shuffle], this.questions[i]];
+    }
+}
 
-    // 6. hasEnded()
+checkAnswer(answer) {
+    if (this.questions[this.currentQuestionIndex].answer === answer){
+        this.correctAnswers += 1;
+        return true;
+    }
+    return false;
+}
+
+hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+        return true;
+    } else {
+        return false; 
+    }
+}
+
+filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+        this.questions = this.questions.filter((question) => { 
+           return question.difficulty === difficulty
+        })
+    }
+}
+
+averageDifficulty() {
+ return this.questions.reduce((acc, question) => {
+        return acc + question.difficulty
+    }, 0) / this.questions.length
+}
+
 }
